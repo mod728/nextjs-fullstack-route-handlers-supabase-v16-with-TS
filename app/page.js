1a74a4d9867a4363023545b1988f275d1ce20d8c
mod728 <phallus-phaser@hotmail.com>
2026-04-18 16:43:13 +0900

    Code for Chapter 8

---
+++ app/page.js
@@ -0,0 +1,31 @@
+import Link from "next/link"
+import Image from "next/image"
+
+export const dynamic = "force-dynamic"
+
+const getAllItems = async() => {
+    const response = await fetch("http://localhost:3000/api/item/readall")
+    const jsonData = await response.json()       
+    const allItems = jsonData.allItems
+    return allItems    
+} 
+
+const ReadAllItems = async() => {
+    const allItems = await getAllItems() 
+    return (
+        <div className="grid-container-in">
+            {allItems.map(item => 
+                <Link href={`/item/readsingle/${item.id}`} key={item.id}>
+                    <Image src={item.image} width={750} height={500} alt="item-image" priority/>
+                    <div> 
+                        <h2>¥{item.price}</h2>
+                        <h3>{item.title}</h3>
+                        <p>{item.description.substring(0, 80)}...</p>  
+                    </div>
+                </Link>
+            )}
+        </div>
+    )
+} 
+
+export default ReadAllItems
