bad9ba2ac24a5f8c1c0284f4e916a393621e78c9
mod728 <phallus-phaser@hotmail.com>
2026-04-18 16:46:40 +0900

    Code for Chapter 9

---
+++ app/api/item/readsingle/[id]/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse, NextRequest } from "next/server"
+import supabase from "../../../../utils/database"
+
+type Context = {                         
+    params: Promise<{ id: string }> 
+}     
+
+export async function GET(request: NextRequest, context: Context){
+    const params = await context.params
+
+    try{
+        const { data, error } = await supabase.from("items")
+                                               .select()
+                                               .eq("id", params.id)
+                                               .single()
+        if(error) throw new Error(error.message) 
+        return NextResponse.json({message: "アイテム読み取り成功（シングル）", singleItem: data})
+    }catch(err){
+        return NextResponse.json({message: `アイテム読み取り失敗（シングル：${err}`})
+    }
+}
